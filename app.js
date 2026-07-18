94fc2cd1efe6db42590efc1dd322803de505f2c5
aashnakhushi12 <khushiaashna5@gmail.com>
2026-07-18 12:05:23 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to User Payout Management System API 🚀",
+  });
+});
+
+module.exports = app;
